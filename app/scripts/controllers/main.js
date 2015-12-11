04aca125f96caa525f924754c7a59428f957b687
Charlott <charlott.vazquez@IG14-VAZQUEZC.intra.eiaj.ch>
2015-12-12 00:09:06 +0100

    1st try : ng-click

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,3 +12,11 @@ angular.module('Foodies')
     var main = this;
     main.api = api;
   });
+
+/*main.showMore = function(elem){
+ var urlPage = "http://localhost:9000/#/detail/" + elem.recipe_id;
+ return $http({
+ method: 'GET',
+ url: urlPage
+ })
+ pas au point encore */
